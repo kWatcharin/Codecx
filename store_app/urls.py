bbedc9dff31b4974308a58eaf65dd5bc7c210ce3
Watcharin Kaewchom <61945492+kWatcharin@users.noreply.github.com>
2022-10-09 20:57:24 +0700

    Add staticfiles and bootstrap5.

--- store_app/urls.py
+++ store_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from store_app import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.store_base_app, name='store_base_app'),
+    path('show_static/', views.show_static, name='show_static')
 ]
 
--- store_app/views.py
+++ store_app/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'store_app/base.html')
+def store_base_app(request):
+    return render(request, 'store_app/components/store_app_base.html')
+
+
+def show_static(request):
+    return render(request, 'store_app/show_static.html')
+
+
 
