572e2b494ea885d547a335d04b07d91a8e7db2b4
Watcharin Kaewchom <61945492+kWatcharin@users.noreply.github.com>
2022-11-16 15:25:20 +0700

    Add cart_app.context_processors.py

--- cart_app/context_processors.py
+++ cart_app/context_processors.py
@@ -2,6 +2,17 @@ from cart_app.cart import Cart
 
 
 def cart(request):
-    return {
-        'cart': Cart(request)
-    }
+
+    cart = Cart(request)
+
+    book_type_qty_in_cart = []
+    for book in cart:
+        book_type_qty_in_cart.append(book['book_id']['quantity'])
+
+    total_books_qty = sum(book_type_qty_in_cart)
+        
+    context = {
+        "cart": cart,
+        "total_books_qty": total_books_qty,
+        }
+    return context
